51e6c5ee2f2808f2005053aa84c4d3833e8ee188
Julia Yi <juliamijiyi@Julia-Yis-MacBook-Air.local>
2017-03-03 19:00:34 -0800

    created game board

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,10 +6,25 @@ var cardFour = "king";
 
 if (cardOne === cardTwo) {
 	alert("You found a match!");
-} else if (cardThree === cardFour) {
+} 
+else if (cardThree === cardFour) {
 	alert("You found a match!");
-} else if (cardOne === cardThree) {
+} 
+else if (cardOne === cardThree) {
 	alert("Sorry, try again.");
-} else if (cardTwo === cardFour) {
+} 
+else if (cardTwo === cardFour) {
 	alert("Sorry, try again.");
 }
+
+var gameBoard = document.getElementById('game-board');
+
+var createCards = function() {
+for (var i = 0; i < 4; i++) {
+	var createCards = document.createElement('div');
+		createCards.className = 'card';
+		gameBoard.appendChild(createCards);
+	}	
+}
+
+createCards(4);
