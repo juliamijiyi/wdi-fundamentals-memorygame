5188fc4b3d47babedc965ffdb87d9726e73e44f7
Julia Yi <juliamijiyi@Julia-Yis-MacBook-Air.local>
2017-03-07 14:42:04 -0800

    finalized memory game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,30 +1,43 @@
-console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-if (cardOne === cardTwo) {
-	alert("You found a match!");
-} 
-else if (cardThree === cardFour) {
-	alert("You found a match!");
-} 
-else if (cardOne === cardThree) {
-	alert("Sorry, try again.");
-} 
-else if (cardTwo === cardFour) {
-	alert("Sorry, try again.");
-}
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
 var gameBoard = document.getElementById('game-board');
 
-var createCards = function() {
-for (var i = 0; i < 4; i++) {
-	var createCards = document.createElement('div');
-		createCards.className = 'card';
-		gameBoard.appendChild(createCards);
-	}	
+//creates card board
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+		gameBoard.appendChild(cardElement);
+		gameBoard.appendChild(cardElement);
+	}
 }
 
-createCards(4);
+//checks to see if two cards match
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
+		alert("You found a match!");
+	} 	else {
+		alert("Sorry, try again.");
+	}
+}
+
+//checks to see if there are cards in play
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+		console.log(this.getAttribute('data-card'));
+		if (this.getAttribute('data-card') === 'king') {
+			this.innerHTML = "<img src='/Users/juliamijiyi/Fundamentals/wdi-fundamentals-memorygame/kingofhearts.png'>";
+		}	else if (this.getAttribute('data-card') === 'queen') {
+			this.innerHTML = "<img src='/Users/juliamijiyi/Fundamentals/wdi-fundamentals-memorygame/queenofhearts.png'>";
+		}
+		if (cardsInPlay.length === 2) {
+			isMatch(cardsInPlay);
+			cardsInPlay = [];
+		}
+}
+
+createBoard();
+
